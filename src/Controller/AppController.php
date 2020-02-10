2820b99cec659642867e963ed009d7b2a6952f04
viniciusassisneves <viniciusassisneves@yahoo.com.br>
2020-02-10 16:34:52 -0300

    small fix

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -45,7 +45,9 @@ class AppController extends Controller
     {
         parent::initialize();
 
-        $this->loadComponent('RequestHandler');
+        $this->loadComponent('RequestHandler', [
+                'enableBeforeRedirect' => false
+        ]);
         $this->loadComponent('Flash');
 
         $this->loadComponent('Auth', [
